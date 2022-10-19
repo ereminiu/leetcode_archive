bb12c1935d51832e487f1dc12516f0924fba6a41
ereminiu <ereminiu@gmail.com>
2022-10-19 16:55:08 +0300

    add task 692

--- solutions/task_0692.py
+++ solutions/task_0692.py
@@ -13,6 +13,3 @@ class Solution:
         for r in range(k):
             ans.append(heapq.heappop(h))
         return ans
-
-print(Solution().topKFrequent(words = ["i","love","leetcode","i","love","coding"], k = 2))
-print(Solution().topKFrequent(words = ["the","day","is","sunny","the","the","the","sunny","is","is"], k = 4))
