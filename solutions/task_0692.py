09e7a35636ed0a607e65db844611cd0641fdfc7a
ereminiu <ereminiu@gmail.com>
2022-10-19 16:50:01 +0300

    First v of task692

--- solutions/task_0692.py
+++ solutions/task_0692.py
@@ -9,7 +9,10 @@ class Solution:
         h = []
         for s in set(words):
             heapq.heappush(h, (counter[s], s))
-        print(h)
+        ans = []
+        for r in range(k):
+            ans.append(heapq.heappop(h))
+        return ans
 
 print(Solution().topKFrequent(words = ["i","love","leetcode","i","love","coding"], k = 2))
 print(Solution().topKFrequent(words = ["the","day","is","sunny","the","the","the","sunny","is","is"], k = 4))
