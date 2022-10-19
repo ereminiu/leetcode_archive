baa29343c66cb002b28190181cbee41498388941
ereminiu <ereminiu@gmail.com>
2022-10-19 16:42:32 +0300

    Top K Frequent words

---
+++ solutions/task_0692.py
@@ -0,0 +1,6 @@
+class Solution:
+    def topKFrequent(self, words, k: int):
+        pass
+
+print(Solution().topKFrequent(words = ["i","love","leetcode","i","love","coding"], k = 2))
+print(Solution().topKFrequent(words = ["the","day","is","sunny","the","the","the","sunny","is","is"], k = 4))
